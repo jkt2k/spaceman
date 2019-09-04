2791465c5af462f2880043e899f1ef134901d935
Jack <jackkatzer@protonmail.com>
2019-09-03 17:58:01 -0700

    wrote main structure of spaceman function

--- spaceman.py
+++ spaceman.py
@@ -1,4 +1,6 @@
 import random
+game_over=False
+incorrect_guesses_left=7
 
 def load_word():
     '''
@@ -65,15 +67,48 @@ def is_guess_in_word(guess, secret_word):
     else:
         return False
 
-
-
-
 def spaceman(secret_word):
     '''
     A function that controls the game of spaceman. Will start spaceman in the command line.
     Args:
       secret_word (string): the secret word to guess.
     '''
+    print("Welcome to spaceman!")
+    print("Spaceman is a guessing game. There is a mystery word which you will try to guess one letter at a time. A placeholder is initially shown, with the number of blanks corresponding to the number of letters in the word. If the letter is in the mystery word, the position(s) of the letter(s) are revealed in the placeholders. Guess the word before you run out of guesses!")
+    print("You win if you can guess the mystery word before the spaceman is drawn. The spaceman is made up of seven parts, and each part is drawn for each incorrect guess. If all seven parts get drawn before you guess the word, then you lose.")
+    #Inspired by Spaceman Project Spec from Make School
+    begin_game=input("Ready? Press enter to begin the game.")
+    guess_number=0
+    letters_guessed=[]
+    global game_over
+    not_yet_guessed=list("abcdefghijklmnopqrstuvwxyz")
+    while game_over==False:
+        print("-------------------------")
+        guess_number=guess_number+1
+        guess=input("Guess "+str(guess_number)+": ")
+        if not len(list(guess))==1:
+            print("You can only enter one letter.")
+            guess_number=guess_number-1
+        else:
+            if guess in letters_guessed:
+                print("You already guessed that letter.")
+                guess_number=guess_number-1
+            else:
+                if is_guess_in_word(guess, secret_word)==True:
+                    print("YES! \""+guess+"\" is in the word.")
+                    letters_guessed.append(guess)
+                    not_yet_guessed.remove(guess)
+                    if is_word_guessed(secret_word,letters_guessed)==True:
+                        print("...YOU GOT IT! The word is "+secret_word+".")
+                        game_over=True
+                    else:
+                        print("Here's what the word looks like now: "+get_guessed_word(secret_word,letters_guessed))
+                        not_yet_guessed_string=""
+                        for letter in not_yet_guessed:
+                            not_yet_guessed_string=not_yet_guessed_string+letter
+                        print("These letters haven't been guessed yet: "+not_yet_guessed_string)
+spaceman(load_word)
+
 
 
     #TODO: show the player information about the game according to the project spec
