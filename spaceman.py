9f3ec2eadf0dc1bb2cde86917b19af98cee83524
Jack <jackkatzer@protonmail.com>
2019-09-10 19:12:00 -0700

    cleaned up

--- spaceman.py
+++ spaceman.py
@@ -4,10 +4,9 @@ game_over=False
 
 def load_word():
     f=open(os.path.dirname(os.path.realpath(__file__))+'/words.txt','r')
-    # credit https://stackoverflow.com/questions/5137497/find-current-directory-and-files-directory
+    #credit https://stackoverflow.com/questions/5137497/find-current-directory-and-files-directory
     words_list = f.readlines()
     f.close()
-
     words_list=words_list[0].split(' ')
     secret_word = random.choice(words_list)
     return secret_word
@@ -34,20 +33,21 @@ def is_guess_in_word(guess, secret_word):
         if guess in list(secret_word):
             return True
     return False
+
 def spaceman(secret_word):
     print("Welcome to Spaceman!")
     print("Spaceman is a guessing game. There is a mystery word which you will try to guess one letter at a time. A placeholder is initially shown, with the number of blanks corresponding to the number of letters in the word. If the letter is in the mystery word, the position(s) of the letter(s) are revealed in the placeholders. Guess the word before you run out of guesses!")
     print("You win if you can guess the mystery word before the spaceman is drawn. The spaceman is made up of seven parts, and each part is drawn for each incorrect guess. If all seven parts get drawn before you guess the word, then you lose.")
     print("Your word contains "+str(len(secret_word))+" letters.")
-    # Inspired by Spaceman Project Spec from Make School
+    #inspired by Spaceman Project Spec from Make School
     begin_game=input("Ready? Press enter to begin the game.")
     guess_number=0
     letters_guessed=[]
-    valid_letters_ascii=list(range(65,91))+list(range(97,123))
+    valid_letters_ascii=list(range(65,91))+list(range(97,123)) #acii ranges for all uppercase and lowercase letters
     global game_over
     incorrect_guesses_left=7
     not_yet_guessed=list("abcdefghijklmnopqrstuvwxyz")
-    while game_over==False:
+    while game_over==False: #perma loop until game is over
         print("-------------------------")
         guess_number=guess_number+1
         print("Here's what the word looks like so far: "+get_guessed_word(secret_word, letters_guessed))
@@ -56,13 +56,13 @@ def spaceman(secret_word):
             not_yet_guessed_string=not_yet_guessed_string+letter
         print("These letters haven't been guessed yet: "+not_yet_guessed_string)
         guess=input("Guess "+str(guess_number)+": ")
-        if not len(list(guess))==1:
+        if not len(list(guess))==1: #input can only be 1 character
             check_for_word=""
             for letter in guess:
                 check_for_word=check_for_word+letter.lower()
-            if check_for_word==secret_word:
+            if check_for_word==secret_word: #...unless they guess the word itself
                 game_over=True
-                print("<!!!!!> YOU WIN!!! You figured out what the word is.")
+                print("[!!!!!] YOU WIN!!! You figured out what the word is.")
             else:
                 print("You can only enter one character.")
                 guess_number=guess_number-1
@@ -70,27 +70,27 @@ def spaceman(secret_word):
             print("Please enter a valid character.")
         else:
             real_guess=guess.lower()
-            if real_guess in letters_guessed:
+            if real_guess in letters_guessed: #check if letter has already been guessed
                 print("You already guessed that letter.")
                 guess_number=guess_number-1
             else:
-                if is_guess_in_word(real_guess, secret_word)==True:
-                    print("<!> YES! \""+real_guess+"\" is in the word.")
+                if is_guess_in_word(real_guess, secret_word)==True: #found new letter in word? do this
+                    print("[!] YES! \""+real_guess.upper()+"\" is in the word.")
                     letters_guessed.append(real_guess)
                     not_yet_guessed.remove(real_guess)
                     if is_word_guessed(secret_word,letters_guessed)==True:
-                        print("<!!!> ...YOU GOT IT! The word is "+secret_word+".")
+                        print("[!!!] ...YOU GOT IT! The word is "+secret_word+".")
                         game_over=True
                     else:
-                        print("Here's what the word looks like now: "+get_guessed_word(secret_word,letters_guessed))
-                else:
-                    print("<!> WRONG! "+real_guess+" is NOT in the word.")
+                        print("LETTER UNCOVERED: "+get_guessed_word(secret_word,letters_guessed))
+                else: #letter not in word
+                    print("[!] WRONG! \""+real_guess.upper()+"\" is NOT in the word.")
                     incorrect_guesses_left=incorrect_guesses_left-1
                     letters_guessed.append(real_guess)
                     not_yet_guessed.remove(real_guess)
                     if incorrect_guesses_left==0:
                         game_over=True
-                        print("<!> YOU LOSE!")
+                        print("[!] YOU LOSE!")
                         print("The word was \""+secret_word+".\"")
                     else:
                         print("You have "+str(incorrect_guesses_left)+" guesses left.")
